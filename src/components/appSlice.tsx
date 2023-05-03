88ec45f7f9271266903daf0b643919a8b6203de6
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-05-03 15:23:17 +0400

    pagination resets after new search

--- src/components/appSlice.tsx
+++ src/components/appSlice.tsx
@@ -1,30 +1,26 @@
 /** @format */
 
-import {
-  createSlice,
-  createSelector,
-  createAsyncThunk,
-} from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "../app/store";
-import data from "./data.json";
+import { createSlice, createSelector, createAsyncThunk } from "@reduxjs/toolkit"
+import type { PayloadAction } from "@reduxjs/toolkit"
+import { RootState } from "../app/store"
+import data from "./data.json"
 
 interface initialStateType {
-  searchValue: string;
-  startDate: Date;
-  endDate: Date;
-  currentPage: number;
-  pageLength: number;
+  searchValue: string
+  startDate: Date
+  endDate: Date
+  currentPage: number
+  pageLength: number
   logStatus: {
-    error: null | string;
-    loading: boolean;
-    success: boolean;
-  };
+    error: null | string
+    loading: boolean
+    success: boolean
+  }
 
   searchResults: {
-    fileName: string;
-    matchedLine: string;
-  }[];
+    fileName: string
+    matchedLine: string
+  }[]
 }
 export const getLogs = createAsyncThunk(
   "logs/get",
@@ -43,7 +39,7 @@ export const getLogs = createAsyncThunk(
           dateTo: endDate.toISOString().split("T")[0],
           searchValue: searchValue,
         })
-    );
+    )
     try {
       // this is to simulate data fetching cuz u didnt faking fix the cors policy
 
@@ -66,23 +62,23 @@ export const getLogs = createAsyncThunk(
           method: "GET",
           headers: { "Content-Type": "application/json" },
         }
-      );
+      )
 
       // code above is the actual api call
 
       // Check if the response is OK and try to parse the text as JSON
       if (res.ok) {
-        const data = await res.text();
-        const jsonData = JSON.parse(data);
-        return jsonData;
+        const data = await res.text()
+        const jsonData = JSON.parse(data)
+        return jsonData
       } else {
-        throw new Error("Failed to fetch data");
+        throw new Error("Failed to fetch data")
       }
     } catch (error) {
-      return rejectWithValue(error);
+      return rejectWithValue(error)
     }
   }
-);
+)
 
 const initialState: initialStateType = {
   searchValue: "",
@@ -97,7 +93,7 @@ const initialState: initialStateType = {
   },
   // add data to search results for testing purposes
   searchResults: [],
-};
+}
 
 const appSlice = createSlice({
   name: "app",
@@ -108,56 +104,56 @@ const appSlice = createSlice({
       state,
       action: PayloadAction<initialStateType["currentPage"]>
     ) => {
-      state.currentPage = action.payload;
+      state.currentPage = action.payload
     },
     setStartDate: (
       state,
       action: PayloadAction<initialStateType["startDate"]>
     ) => {
-      state.startDate = action.payload;
+      state.startDate = action.payload
     },
     setEndDate: (state, action: PayloadAction<initialStateType["endDate"]>) => {
-      state.endDate = action.payload;
+      state.endDate = action.payload
     },
     setSearchValue: (
       state,
       action: PayloadAction<initialStateType["searchValue"]>
     ) => {
-      state.searchValue = action.payload;
+      state.searchValue = action.payload
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getLogs.pending, (state) => {
-        state.logStatus.loading = true;
-        state.logStatus.success = false;
-        state.logStatus.error = null;
+        state.logStatus.loading = true
+        state.logStatus.success = false
+        state.logStatus.error = null
       })
       .addCase(getLogs.fulfilled, (state, action: any) => {
-        console.log("action.payload:", action.payload);
+        console.log("action.payload:", action.payload)
         state.searchResults = action.payload.filter(
           (item: { fileName: string; matchedLine: string }) =>
             item.fileName.includes("ChatLog")
-        ); // Directly use the parsed data and filter out anything that isnt chatlog because apperantly noone gives a goose neck about datatructures
-        state.searchValue = "";
-        state.logStatus.loading = false;
+        ) // Directly use the parsed data and filter out anything that isnt chatlog because apperantly noone gives a goose neck about datatructures
+        state.searchValue = ""
+        state.currentPage = 0
+        state.logStatus.loading = false
       })
       .addCase(getLogs.rejected, (state, action: any) => {
-        state.searchValue = "";
-        state.logStatus.loading = false;
-        state.logStatus.error =
-          "Error fetching data: " + action.payload.message;
-      });
+        state.searchValue = ""
+        state.logStatus.loading = false
+        state.logStatus.error = "Error fetching data: " + action.payload.message
+      })
   },
-});
+})
 
 export const selectPageLength = createSelector(
   (state: RootState) => state.app,
   (app) => {
-    return Math.ceil(app.searchResults.length / app.pageLength);
+    return Math.ceil(app.searchResults.length / app.pageLength)
   }
-);
+)
 
 export const { setStartDate, setEndDate, setCurrentPage, setSearchValue } =
-  appSlice.actions;
-export default appSlice.reducer;
+  appSlice.actions
+export default appSlice.reducer
